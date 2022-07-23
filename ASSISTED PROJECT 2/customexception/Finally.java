d6f77e9e3844fc9383d42bb308ec0b837626165d
umayalan <109449811+umayalan@users.noreply.github.com>
2022-07-23 15:55:02 +0530

    Add files via upload

---
+++ ASSISTED PROJECT 2/customexception/Finally.java
@@ -0,0 +1,31 @@
+package com.simplilearn.customexception;
+
+
+	
+
+	import java.util.InputMismatchException;
+	import java.util.Scanner;
+
+	public class Finally {
+
+		public static void main(String[] args) {
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter value");
+			
+			try {
+				
+				int num= sc.nextInt();
+				System.out.println("Value: "+num);
+				
+			} catch (InputMismatchException e) {
+				// TODO: handle exception
+				System.out.println("Wrong input");
+			}
+			finally {
+				sc.close();
+				System.out.println("Scanner Closed Successfully");
+			}
+			
+		}
+	}
+
