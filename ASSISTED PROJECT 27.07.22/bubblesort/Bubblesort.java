ad7881044f20048a3ad5085b772ac2dfae9d2193
umayalan <109449811+umayalan@users.noreply.github.com>
2022-07-27 18:47:34 +0530

    assisted project 4

---
+++ ASSISTED PROJECT 27.07.22/bubblesort/Bubblesort.java
@@ -0,0 +1,38 @@
+package com.simplilearn.bubblesort;
+
+
+	public class Bubblesort {
+
+
+	    public static void main(String[] args){
+
+	     int[] arr= {30,25,20,5,10};
+	     bubbleSort(arr);
+	     for(int i=0;i<arr.length;i++){
+
+	        System.out.println(arr[i]);
+	        }
+	    }
+
+	    public static void bubbleSort(int[] arr){
+	        int len = arr.length;
+	        int temp = 0;
+	        for(int i=0;i<len;i++){
+	            for (int j=1;j<(len);j++){
+	                if(arr[j-1]>arr[j]){
+	                temp = arr[j-1];
+	                arr[j-1]= arr[j];
+	                arr[j]= temp;
+
+	                }
+
+
+	            }
+
+	        }
+
+	    }
+
+	}
+
+
---
+++ ASSISTED PROJECT 27.07.22/insertionsort/Insertionsort.java
@@ -0,0 +1,33 @@
+package com.simplilearn.insertionsort;
+
+public class Insertionsort {
+	
+
+	    public static  void main(String[] args){
+
+	        int[] arr = {8,12,4,27,44};
+	        insertionSort(arr);
+	        for(int i=0;i<arr.length;i++){
+
+	            System.out.println(arr[i]);
+
+	        }
+	     }
+	    public static void insertionSort(int[] arr){
+
+	    int len = arr.length;
+	    for(int j=1;j<len;j++){
+	    int key = arr[j];
+	    int i=j-1;
+	    while ((i>-1) && (arr[i]>key)){
+
+	        arr[i+1]=arr[i];
+	        i--;
+	    }
+	    arr[i+1]=key;
+	         }
+
+	    }
+	}
+
+
