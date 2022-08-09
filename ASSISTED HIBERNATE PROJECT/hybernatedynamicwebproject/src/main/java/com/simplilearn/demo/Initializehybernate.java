33fea30fcd9852cc32eac0572cb21722afa49f7a
umayalan <109449811+umayalan@users.noreply.github.com>
2022-08-09 13:10:11 +0530

    Add files via upload

---
+++ ASSISTED HIBERNATE PROJECT/hybernatedynamicwebproject/src/main/java/com/simplilearn/demo/Initializehybernate.java
@@ -0,0 +1,47 @@
+package com.simplilearn.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+@WebServlet("/init")
+public class Initializehybernate extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		PrintWriter out= resp.getWriter();
+		
+		SessionFactory factory= Hibernateutils.getSessionFactory();
+		Session session= factory.openSession();
+		
+		out.print("Session is Opened");
+		
+		session.close();
+		
+		out.print("Session is closed");
+		
+		
+		
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(req, resp);
+	}
+	
+	
+
+}
+
