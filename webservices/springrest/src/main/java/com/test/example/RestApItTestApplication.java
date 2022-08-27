b76f53c70aeff64dc7176eea5eab7b3682c87ef0
umayalan <109449811+umayalan@users.noreply.github.com>
2022-08-27 15:34:04 +0530

    WEBSERVICES ASSISTED PRACTISE,RESTAPI,REST CLIENT

---
+++ webservices/springrest/src/main/java/com/test/example/RestApItTestApplication.java
@@ -0,0 +1,14 @@
+
+package com.test.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApItTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApItTestApplication.class, args);
+    }
+
+}
