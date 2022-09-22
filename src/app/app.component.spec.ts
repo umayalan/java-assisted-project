257115da330b8b9bceea5f9df0e7e43b8c8d531a
umayalan <109449811+umayalan@users.noreply.github.com>
2022-09-22 06:08:30 +0100

    DIRECTIVES.JS

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ClassAndStyle'`, () => {
+  it(`should have as title 'directives'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ClassAndStyle');
+    expect(app.title).toEqual('directives');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ClassAndStyle app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('directives app is running!');
   });
 });
